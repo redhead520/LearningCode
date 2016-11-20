51d335e78f9ff17bc2dea647d2497d6fdaed3163
hhf <jxhuanghf@gmail.com>
2016-11-21 00:03:05 +0800

    tt

---
+++ python/demo/01.dataStructure-demo/01.dataStructure-demo.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+
+a = 10 / 3
+b = 10 // 3
+c = 10 % 3
+
+d = 10 / 3.0
+e = 10 // 3.0
+f = 10.5 % 3.0
+
+print(a, b, c)
+print(d, e, f)
+print "-----------------------"
