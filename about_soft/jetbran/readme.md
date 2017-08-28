47190bd6b6f6c679b25dff98aca774246790293c
redhead <hongfa.huang@moxi.xyz>
2017-08-28 19:45:09 +0800

    new

---
+++ about_soft/jetbran/readme.md
@@ -0,0 +1,12 @@
+## jetbrains 注册服务器搭建:
+
+- 一台VPS
+- 一键脚本
+
+1. 建议在特权模式下运行，即sudo -s一下
+    ```commandline
+    wget http://home.ustc.edu.cn/~mmmwhy/jetbrain.sh && sh ./jetbrain.sh
+    
+    ```
+2. 会出现两个空，用于输入端口号和默认用户名，建议直接回车，否则可能会出现莫名其妙的错误
+3. 讲最后那个License sever地址记下，之后激活用
--- python/demo/test.py
+++ python/demo/test.py
@@ -2,21 +2,12 @@
 # -*- coding: utf-8 -*-
 #    9/2/16
 
-" a module "
 
-__author__ = 'redhead'
-
-
-# metaclass是创建类，所以必须从`type`类型派生：
-class ListMetaclass(type):
-    def __new__(cls, name, bases, attrs):
-        attrs['add'] = lambda self, value: self.append(value)
-        return type.__new__(cls, name, bases, attrs)
-
-
-class MyList (list):
-    __metaclass__ = ListMetaclass  # 指示使用ListMetaclass来定制类
-
-h = MyList()
-h.add(2)
-print(h)
+a = 1
+# try:
+#     print 'try'
+# except:
+#     print 'except'
+# else:
+#     print 'else'
+print a
