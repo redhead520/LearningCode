ffb238d4a17999e5696e7e087a2f00b23352aef4
redhead <hongfa.huang@moxi.xyz>
2017-12-16 22:34:10 +0800

    del liaoxuefeng project

--- python/demo/liaoxuefeng_project/py2-webapp/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-' a test module '
-
-__author__ = 'redhead'
-
-if __name__ == '__main__':
-    pass
--- python/demo/liaoxuefeng_project/py2-webapp/conf/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-from . import config_override
-from . import config_default
-from . import config
